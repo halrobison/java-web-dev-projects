3383648306d1ce0a5d97db2562f37df6eefda9a8
halrobison <halrobisoncreative@gmail.com>
2024-04-07 17:38:15 -0500

    implemented sumEven in ArrayListPractice

--- control-flow-and-collections/exercises/src/main/java/org/launchcode/ArrayListPractice.java
+++ control-flow-and-collections/exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -3,6 +3,16 @@ import java.util.ArrayList;
 
 
 public class ArrayListPractice {
+
+        public static int main(String[] args){
+            int[] integerArray = {1, 1, 2, 3, 5, 8, 7, 8, 9, 1};
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for(int item : integerArray){
+                arrayList.add(item);
+            }
+            // Ask Phillip: why can't I initialize like "ArrayList<int>" why do I need to use "ArrayList<Integer>"
+            int total = sumEven(arrayList);
+        }
         public static int sumEven(ArrayList<Integer> arr) {
             int total = 0;
             for (int integer : arr) {
