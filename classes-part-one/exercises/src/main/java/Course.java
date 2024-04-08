e6e2831da4128eea15970f02f59250b347e4c037
halrobison <halrobisoncreative@gmail.com>
2024-04-08 18:30:36 -0500

    classes exercise lecture review

--- classes-part-one/exercises/src/main/java/Course.java
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -1,5 +1,29 @@
 public class Course {
     private String topic;
     private StudentPractice.Teacher instructor;
-    private ArrayList<Student> enrolledStudents;
+
+    public Course (String topic, StudentPractice.Teacher instructor) {
+        this.topic = topic;
+        this.instructor = instructor;
+    }
+
+    public Course(String worldHistory, Teacher historyTeacher) {
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public StudentPractice.Teacher getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(StudentPractice.Teacher instructor) {
+        this.instructor = instructor;
+    }
+
 }
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,6 +1,11 @@
 public class StudentPractice {
     public static void main(String[] args){
         //instantiate your Student class below
-        Student student = new Student("hal", 64114, 1, 4.0)
+        Student student = new Student("Hal Robison", 64114, 1, 4.0);
+        Teacher historyTeacher = new Teacher("Phillip", "Hodges", "history", 15);
+        Course worldHistory = new Course("World History", historyTeacher);
+
+        System.out.println("NAME: " + student.getName());
+        System.out.println("ID: " + student.getStudentId());
     }
 }
